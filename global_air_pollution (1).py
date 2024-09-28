c3c83fed2e4e46fc41bcb9cb60f955432b8c3b39
ngolehoangkim <kimnlh22413@st.uel.edu.vn>
2024-09-28 08:46:43 +0700

    demohoy.py

--- global_air_pollution (1).py
+++ global_air_pollution (1).py
@@ -1,8 +1,18 @@
+import pandas as pd
+import numpy as np
+
+# Tạo dữ liệu ngẫu nhiên cho x và y
+x = np.arange(0, 10)  # Dữ liệu x từ 0 đến 9
+y = np.random.randint(0, 100, size=10)  # Dữ liệu y ngẫu nhiên từ 0 đến 99
+
+# Tạo DataFrame với các cột x và y
+data = pd.DataFrame({'x': x, 'y': y})
+
 import seaborn as sns
 import matplotlib.pyplot as plt
 plt.figure(figsize=(10, 5))
-sns.histplot(data['AQI Value'], bins=30, kde=True, color='#DDA0DD')
-plt.xlabel('AQI Value')
-plt.ylabel('Value')
-plt.title('Histogram of AQI Value')
+sns.histplot(data, bins=30, kde=True, color='#DDA0DD')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('demo hoy')
 plt.show()
